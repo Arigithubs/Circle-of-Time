a22e6dc18b1abcdf9cc9f328eade5608e65fee60
Arigithubs <arimail@duck.com>
2023-12-26 11:44:17 -0500

    Refactor task form and task element creation

--- script.js
+++ script.js
@@ -3,13 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const taskForm = document.getElementById('taskForm');
     const taskNameInput = document.getElementById('taskName');
     const taskDurationInput = document.getElementById('taskDuration');
-    const taskTimeInput = document.getElementById('taskTime');
     const taskColorInput = document.getElementById('taskColor');
     const colorPalette = document.getElementById('colorPalette');
-    const colorOptions = colorPalette.querySelectorAll('.color-option');
-    const addTaskButton = document.getElementById('addTaskButton');
-    const cancelButton = document.getElementById('cancelButton');
     const saveButton = document.getElementById('saveButton');
+    const cancelButton = document.getElementById('cancelButton');
 
     let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
@@ -22,8 +19,6 @@ document.addEventListener('DOMContentLoaded', () => {
             const taskElement = createTaskElement(task);
             taskElement.addEventListener('click', () => editTask(task));
             taskElement.addEventListener('contextmenu', event => deleteTask(event, task));
-            taskElement.addEventListener('mouseover', () => showTooltip(taskElement, task));
-            taskElement.addEventListener('mouseout', () => hideTooltip(taskElement));
             circle.appendChild(taskElement);
         });
 
@@ -34,27 +29,11 @@ document.addEventListener('DOMContentLoaded', () => {
         const taskElement = document.createElement('div');
         taskElement.classList.add('task');
         taskElement.style.backgroundColor = task.color;
-        const rotateDegree = calculateRotation(task);
-        taskElement.style.transform = `rotate(${rotateDegree}deg)`;
         const taskContent = document.createElement('div');
         taskContent.classList.add('task-content');
-        taskContent.innerHTML = `<span>${task.name}</span><br><span class="task-time">${task.time}</span>`;
+        taskContent.innerHTML = `<span>${task.name}</span><br><span>${task.duration} hours</span>`;
         taskElement.appendChild(taskContent);
         return taskElement;
-    };
-
-    const calculateRotation = task => {
-        const existingRotations = tasks.map(t => (t.duration / 24) * 360);
-        const taskRotation = (task.duration / 24) * 360;
-        let rotateDegree = taskRotation;
-        let index = 1;
-
-        while (existingRotations.includes(rotateDegree)) {
-            rotateDegree = taskRotation + index * 5;
-            index++;
-        }
-
-        return rotateDegree;
     };
 
     const openTaskForm = () => {
@@ -67,10 +46,10 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const saveTask = () => {
-        const { name, duration, time, color } = getInputValues();
+        const { name, duration, color } = getInputValues();
 
-        if (isValidTask(name, duration, time, color)) {
-            const newTask = { name, duration, time, color };
+        if (isValidTask(name, duration, color)) {
+            const newTask = { name, duration, color };
             tasks.push(newTask);
             animateTask(newTask, 'add');
             renderTasks();
@@ -89,12 +68,11 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     const saveEditedTask = task => {
-        const { name, duration, time, color } = getInputValues();
+        const { name, duration, color } = getInputValues();
 
-        if (isValidTask(name, duration, time, color)) {
+        if (isValidTask(name, duration, color)) {
             task.name = name;
             task.duration = duration;
-            task.time = time;
             task.color = color;
             renderTasks();
             closeTaskForm();
@@ -138,84 +116,51 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 
-    if (addTaskButton) {
-        addTaskButton.addEventListener('click', openTaskForm);
+    if (saveButton) {
+        saveButton.addEventListener('click', saveTask);
     }
 
     if (cancelButton) {
         cancelButton.addEventListener('click', closeTaskForm);
     }
 
-    if (saveButton) {
-        saveButton.addEventListener('click', saveTask);
-    }
-
-    window.addEventListener('resize', () => renderTasks());
-
     renderTasks();
 
     const getInputValues = () => ({
         name: taskNameInput.value,
         duration: parseFloat(taskDurationInput.value),
-        time: taskTimeInput.value,
         color: taskColorInput.value,
     });
 
     const setInputValues = task => {
         taskNameInput.value = task.name;
         taskDurationInput.value = task.duration;
-        taskTimeInput.value = task.time;
         taskColorInput.value = task.color;
         selectColorOption(task.color);
     };
 
-    const isValidTask = (name, duration, time, color) =>
-        name && !isNaN(duration) && duration > 0 && isValidTime(time) && color;
-
-    const isValidTime = time => {
-        const regex = /^(0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
-        return regex.test(time);
-    };
+    const isValidTask = (name, duration, color) =>
+        name && !isNaN(duration) && duration > 0 && color;
 
     const clearForm = () => {
         taskNameInput.value = '';
         taskDurationInput.value = '';
-        taskTimeInput.value = '';
-        taskColorInput.value = colorOptions[0].dataset.color;
-        selectColorOption(colorOptions[0].dataset.color);
-    };
-
-    const updateColorPalette = () => {
-        const usedColors = tasks.map(task => task.color);
-        colorOptions.forEach(option => {
-            const color = option.dataset.color;
-            option.style.display = usedColors.includes(color) ? 'none' : 'block';
-        });
+        taskColorInput.value = '#e74c3c'; // Default color
+        selectColorOption('#e74c3c');
     };
 
     const selectColorOption = color => {
-        colorOptions.forEach(option => {
-            option.classList.remove('selected');
-            if (option.dataset.color === color) {
-                option.classList.add('selected');
-                option.style.transform = 'scale(1.2)';
-            } else {
-                option.style.transform = 'scale(1)';
-            }
-        });
-    };
+        const selectedColor = colorPalette.querySelector('.selected');
+        if (selectedColor) {
+            selectedColor.classList.remove('selected');
+        }
 
-    const showTooltip = (taskElement, task) => {
-        const tooltip = document.createElement('div');
-        tooltip.classList.add('tooltip');
-        tooltip.innerHTML = `<span>${task.name}</span><br><span>${task.duration} hours at ${task.time}</span>`;
-        taskElement.appendChild(tooltip);
-    };
+        const colorOption = Array.from(colorPalette.children).find(option =>
+            option.dataset.color === color
+        );
 
-    const hideTooltip = taskElement => {
-        const tooltip = taskElement.querySelector('.tooltip');
-        if (tooltip) {
-            tooltip.remove();
+        if (colorOption) {
+            colorOption.classList.add('selected');
         }
     };
 });
