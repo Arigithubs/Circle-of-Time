b5699cb35cdd5efab8673136f4c7808b4a2f965d
Arigithubs <arimail@duck.com>
2023-12-26 10:19:49 -0500

    Refactor task rendering and editing functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', () => {
     // Elements
     const circle = document.getElementById('circle');
     const taskForm = document.getElementById('taskForm');
@@ -15,89 +15,84 @@ document.addEventListener('DOMContentLoaded', function () {
         // Add more sample tasks as needed
     ];
 
-    // Function to render tasks on the circle
-    function renderTasks() {
+    // Render tasks on the circle
+    const renderTasks = () => {
         circle.innerHTML = '';
         tasks.forEach(task => {
-            const taskElement = document.createElement('div');
-            taskElement.classList.add('task');
-            taskElement.style.backgroundColor = task.color;
-            taskElement.style.transform = `rotate(${(task.duration / 24) * 360}deg)`;
+            const taskElement = createTaskElement(task);
             taskElement.addEventListener('click', () => editTask(task));
             circle.appendChild(taskElement);
         });
 
-        // Save tasks to local storage
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }
+        saveTasksToLocalStorage();
+    };
 
-    // Function to open the task form
-    function openTaskForm() {
-        taskForm.style.display = 'block';
-    }
+    // Create a task element
+    const createTaskElement = task => {
+        const taskElement = document.createElement('div');
+        taskElement.classList.add('task');
+        taskElement.style.backgroundColor = task.color;
+        taskElement.style.transform = `rotate(${(task.duration / 24) * 360}deg)`;
+        return taskElement;
+    };
 
-    // Function to close the task form
-    function closeTaskForm() {
-        taskForm.style.display = 'none';
-    }
+    // Open the task form
+    const openTaskForm = () => taskForm.style.display = 'block';
 
-    // Function to save a new task
-    function saveTask() {
-        const taskName = taskNameInput.value;
-        const taskDuration = parseInt(taskDurationInput.value);
-        const taskColor = taskColorInput.value;
+    // Close the task form
+    const closeTaskForm = () => taskForm.style.display = 'none';
 
-        if (taskName && !isNaN(taskDuration) && taskDuration > 0 && taskColor) {
-            const newTask = { name: taskName, duration: taskDuration, color: taskColor };
+    // Save a new task
+    const saveTask = () => {
+        const { name, duration, color } = getInputValues();
+
+        if (isValidTask(name, duration, color)) {
+            const newTask = { name, duration, color };
             tasks.push(newTask);
             renderTasks();
             closeTaskForm();
         } else {
             alert('Please fill in all fields with valid values.');
         }
-    }
+    };
 
-    // Function to edit an existing task
-    function editTask(task) {
-        taskNameInput.value = task.name;
-        taskDurationInput.value = task.duration;
-        taskColorInput.value = task.color;
+    // Edit an existing task
+    const editTask = task => {
+        setInputValues(task);
         selectColorOption(task.color);
         openTaskForm();
 
         saveButton.removeEventListener('click', saveTask);
         saveButton.addEventListener('click', () => saveEditedTask(task));
-    }
+    };
 
-    // Function to save edits to an existing task
-    function saveEditedTask(task) {
-        const updatedName = taskNameInput.value;
-        const updatedDuration = parseInt(taskDurationInput.value);
-        const updatedColor = taskColorInput.value;
+    // Save edits to an existing task
+    const saveEditedTask = task => {
+        const { name, duration, color } = getInputValues();
 
-        if (updatedName && !isNaN(updatedDuration) && updatedDuration > 0 && updatedColor) {
-            task.name = updatedName;
-            task.duration = updatedDuration;
-            task.color = updatedColor;
+        if (isValidTask(name, duration, color)) {
+            task.name = name;
+            task.duration = duration;
+            task.color = color;
             renderTasks();
             closeTaskForm();
         } else {
             alert('Please fill in all fields with valid values.');
         }
-    }
+    };
 
-    // Function to delete a task
-    function deleteTask(task) {
+    // Delete a task
+    const deleteTask = task => {
         const taskIndex = tasks.indexOf(task);
         if (taskIndex !== -1) {
             tasks.splice(taskIndex, 1);
             renderTasks();
         }
-    }
+    };
 
-    // Event listener for right-click to delete a task
-    circle.addEventListener('contextmenu', function (event) {
-        event.preventDefault(); // Prevent the default context menu
+    // Right-click to delete a task
+    circle.addEventListener('contextmenu', event => {
+        event.preventDefault();
         const clickedTask = event.target.closest('.task');
         if (clickedTask) {
             const task = tasks.find(t => t.color === clickedTask.style.backgroundColor);
@@ -105,8 +100,8 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Function to highlight the selected color option
-    function selectColorOption(color) {
+    // Highlight the selected color option
+    const selectColorOption = color => {
         colorOptions.forEach(option => {
             option.classList.remove('selected');
             if (option.dataset.color === color) {
@@ -116,10 +111,10 @@ document.addEventListener('DOMContentLoaded', function () {
                 option.style.transform = 'scale(1)';
             }
         });
-    }
+    };
 
-    // Event listener for clicking a color option
-    colorPalette.addEventListener('click', function (event) {
+    // Click a color option
+    colorPalette.addEventListener('click', event => {
         const colorOption = event.target.closest('.color-option');
         if (colorOption) {
             taskColorInput.value = colorOption.dataset.color;
@@ -134,4 +129,24 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Initial rendering of tasks
     renderTasks();
+
+    // Helper functions
+
+    const getInputValues = () => ({
+        name: taskNameInput.value,
+        duration: parseInt(taskDurationInput.value),
+        color: taskColorInput.value
+    });
+
+    const setInputValues = task => {
+        taskNameInput.value = task.name;
+        taskDurationInput.value = task.duration;
+        taskColorInput.value = task.color;
+    };
+
+    const isValidTask = (name, duration, color) =>
+        name && !isNaN(duration) && duration > 0 && color;
+
+    const saveTasksToLocalStorage = () =>
+        localStorage.setItem('tasks', JSON.stringify(tasks));
 });
